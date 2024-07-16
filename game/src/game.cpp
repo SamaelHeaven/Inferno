604c34c48e0a8480c79c20309867ba24552e3682
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-07-16 19:54:06 -0400

    Separating engine from game

--- game/src/game.cpp
+++
@@ -1,9 +0,0 @@
-#include "./scenes/MainScene.h"
-
-void main() {
-    GameConfig config;
-    config.title = "Game";
-    config.icon = "textures/icon.png";
-    file::set_working_directory("assets");
-    Game::launch<MainScene>(config);
-}
--- game/src/game.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <inferno.h>
-
-using namespace inferno;
