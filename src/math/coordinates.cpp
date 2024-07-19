a33df94e8039898cd9baefc0d911a9e3bafa67d1
Indatou <indatou@icible.ca>
2024-07-19 13:09:36 -0400

    WIP Mouse

--- src/math/coordinates.cpp
+++ src/math/coordinates.cpp
@@ -7,8 +7,7 @@ namespace inferno::coordinates {
         const auto size = Game::get_size();
         const auto screen_size = Game::get_screen_size();
         const auto scale = std::min(screen_size.x / size.x, screen_size.y / size.y);
-        coordinates.x -= (screen_size.x - size.x * scale) * 0.5f;
-        coordinates.y -= (screen_size.y - size.y * scale) * 0.5f;
+        coordinates -= (screen_size - size * scale) * 0.5;
         coordinates /= scale;
         return coordinates;
     }
@@ -18,8 +17,7 @@ namespace inferno::coordinates {
         const auto screen_size = Game::get_screen_size();
         const auto scale = std::min(screen_size.x / size.x, screen_size.y / size.y);
         coordinates *= scale;
-        coordinates.x += (screen_size.x - size.x * scale) * 0.5f;
-        coordinates.y += (screen_size.y - size.y * scale) * 0.5f;
+        coordinates += (screen_size - size * scale) * 0.5;
         return coordinates;
     }
 }
