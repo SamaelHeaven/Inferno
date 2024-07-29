e08e45003d67d85ac370c7f738cdac3a62eddd68
Indatou <indatou@icible.ca>
2024-07-29 14:41:11 -0400

    Refactor: Minor changes

--- src/inferno.cpp
+++ src/inferno.cpp
@@ -2,11 +2,11 @@
 
 #undef main
 
-int32_t inferno_main(std::list<std::string> args);
+int32_t inferno_main(std::vector<std::string> args);
 
 int32_t main(const int32_t argc, const char **argv) {
     SetTraceLogLevel(inferno::internal::LOG_NONE);
-    std::list<std::string> args;
+    std::vector<std::string> args;
     for (auto i = 0; i < argc; i++) {
         args.emplace_back(argv[i]);
     }
