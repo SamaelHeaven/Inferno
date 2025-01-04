46697a01b6421cbc3e016b08e08baa927cd4ab00
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-04 18:21:19 -0500

    WIP: ECS

--- src/core/ECS.h
+++ src/core/ECS.h
@@ -36,7 +36,7 @@ namespace inferno {
         }
 
         template <typename... T, typename Callback> void forEach(Callback &&callback) {
-            registry_.view<T...>().each([callback]<typename... Components>(auto entity, auto &...components) {
+            registry_.view<T...>().each([callback](auto entity, auto &...components) {
                 if constexpr (std::is_invocable_v<Callback, Entity, T...>) {
                     callback(static_cast<Entity>(entity), components...);
                 } else {
