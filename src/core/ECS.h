5063e1b02552bef496aa1bfd35feb504d0db2b59
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-05 01:37:16 -0500

    Feat: ECS

--- src/core/ECS.h
+++ src/core/ECS.h
@@ -76,7 +76,7 @@ namespace inferno {
 
         template <typename... T> std::vector<std::tuple<Entity, T...>> find() {
             const auto view = registry_.view<T...>();
-            std::vector<std::tuple<Entity, T...>> results(view.capacity());
+            std::vector<std::tuple<Entity, T...>> results;
             view.each([&](auto entity, auto &...components) {
                 results.emplace_back(static_cast<Entity>(entity), components...);
             });
