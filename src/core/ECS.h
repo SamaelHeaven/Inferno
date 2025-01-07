75fc30e9174b5ba38bb67dc005fd53c9a2dad2db
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-07 03:22:15 -0500

    Refactor: Minor changes

--- src/core/ECS.h
+++ src/core/ECS.h
@@ -25,9 +25,9 @@ namespace inferno {
 
         void on_fixed_update(const FixedUpdateListener &fixed_update_listener);
 
-        template <typename T> consteval void on_add(constexpr void (*callback)(Entity, T &)) {
-            constexpr auto candidate = [callback](entt::entity entity, entt::registry &, auto &component) {
-                callback(static_cast<Entity>(entity), component);
+        template <typename T, void (*Callback)(Entity, T &)> consteval void on_add() {
+            constexpr auto candidate = [](entt::entity entity, entt::registry &, auto &component) {
+                Callback(static_cast<Entity>(entity), component);
             };
             registry_.on_construct<T>().template connect<candidate>();
         }
