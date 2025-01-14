04a3be3502c7bb67c24497219e4c61535262ec9b
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-14 16:16:42 -0500

    Refactor: Minor changes

--- src/math/Random.cpp
+++ src/math/Random.cpp
@@ -7,57 +7,57 @@ namespace inferno {
         engine_ = std::mt19937(this->seed);
     }
 
-    int8_t Random::int8(const int8_t low, const int8_t high) {
+    int8_t Random::int8(const int8_t low, const int8_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    int16_t Random::int16(const int16_t low, const int16_t high) {
+    int16_t Random::int16(const int16_t low, const int16_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    int32_t Random::int32(const int32_t low, const int32_t high) {
+    int32_t Random::int32(const int32_t low, const int32_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    int64_t Random::int64(const int64_t low, const int64_t high) {
+    int64_t Random::int64(const int64_t low, const int64_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    uint8_t Random::uint8(const uint8_t low, const uint8_t high) {
+    uint8_t Random::uint8(const uint8_t low, const uint8_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    uint16_t Random::uint16(const uint16_t low, const uint16_t high) {
+    uint16_t Random::uint16(const uint16_t low, const uint16_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    uint32_t Random::uint32(const uint32_t low, const uint32_t high) {
+    uint32_t Random::uint32(const uint32_t low, const uint32_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    uint64_t Random::uint64(const uint64_t low, const uint64_t high) {
+    uint64_t Random::uint64(const uint64_t low, const uint64_t high) const {
         std::uniform_int_distribution dist(low, high);
         return dist(engine_);
     }
 
-    float Random::float32(const float low, const float high) {
+    float Random::float32(const float low, const float high) const {
         std::uniform_real_distribution dist(low, high);
         return dist(engine_);
     }
 
-    double Random::float64(const double low, const double high) {
+    double Random::float64(const double low, const double high) const {
         std::uniform_real_distribution dist(low, high);
         return dist(engine_);
     }
 
-    bool Random::boolean() {
+    bool Random::boolean() const {
         std::uniform_int_distribution dist(0, 1);
         return dist(engine_);
     }
--- src/math/Random.h
+++ src/math/Random.h
@@ -9,27 +9,27 @@ namespace inferno {
 
         const uint32_t seed;
 
-        int8_t int8(int8_t low, int8_t high);
+        [[nodiscard]] int8_t int8(int8_t low, int8_t high) const;
 
-        int16_t int16(int16_t low, int16_t high);
+        [[nodiscard]] int16_t int16(int16_t low, int16_t high) const;
 
-        int32_t int32(int32_t low, int32_t high);
+        [[nodiscard]] int32_t int32(int32_t low, int32_t high) const;
 
-        int64_t int64(int64_t low, int64_t high);
+        [[nodiscard]] int64_t int64(int64_t low, int64_t high) const;
 
-        uint8_t uint8(uint8_t low, uint8_t high);
+        [[nodiscard]] uint8_t uint8(uint8_t low, uint8_t high) const;
 
-        uint16_t uint16(uint16_t low, uint16_t high);
+        [[nodiscard]] uint16_t uint16(uint16_t low, uint16_t high) const;
 
-        uint32_t uint32(uint32_t low, uint32_t high);
+        [[nodiscard]] uint32_t uint32(uint32_t low, uint32_t high) const;
 
-        uint64_t uint64(uint64_t low, uint64_t high);
+        [[nodiscard]] uint64_t uint64(uint64_t low, uint64_t high) const;
 
-        float float32(float low, float high);
+        [[nodiscard]] float float32(float low, float high) const;
 
-        double float64(double low, double high);
+        [[nodiscard]] double float64(double low, double high) const;
 
-        bool boolean();
+        [[nodiscard]] bool boolean() const;
 
     private:
         std::mt19937 engine_;
