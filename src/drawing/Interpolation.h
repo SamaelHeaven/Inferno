f6b8f191a43e3aab9bddafc0fd38570805f9a0cb
Samuel Paradis <samuelparadisinfo@gmail.com>
2025-01-14 17:46:10 -0500

    Feat: Interpolation

--- src/drawing/Interpolation.h
+++ src/drawing/Interpolation.h
@@ -6,6 +6,9 @@ namespace inferno {
     enum class Interpolation {
         NONE = internal::TextureFilter::TEXTURE_FILTER_POINT,
         BILINEAR = internal::TextureFilter::TEXTURE_FILTER_BILINEAR,
-        TRILINEAR = internal::TextureFilter::TEXTURE_FILTER_TRILINEAR
+        TRILINEAR = internal::TextureFilter::TEXTURE_FILTER_TRILINEAR,
+        ANISOTROPIC_4X = internal::TextureFilter::TEXTURE_FILTER_ANISOTROPIC_4X,
+        ANISOTROPIC_8X = internal::TextureFilter::TEXTURE_FILTER_ANISOTROPIC_8X,
+        ANISOTROPIC_16X = internal::TextureFilter::TEXTURE_FILTER_ANISOTROPIC_16X
     };
 }
